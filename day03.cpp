870a100d33806c301b268613b0279cb8c7e6e5cf
amorphous1 <harrystew@gmail.com>
2022-12-03 18:42:04 +0000

    refactor day 3 solutions for brevity

--- day03.cpp
+++ day03.cpp
@@ -5,48 +5,38 @@
 
 using namespace std;
 
+set<char> common_chars(const string& s1, const string& s2) {
+    set<char> chars1(s1.begin(), s1.end()), chars2(s2.begin(), s2.end()), common;
+    set_intersection(chars1.begin(), chars1.end(), chars2.begin(), chars2.end(), inserter(common, common.begin()));
+    return common;
+}
+set<char> common_chars(const string& s1, const string& s2, const string& s3) {
+    set<char> common12 = common_chars(s1, s2);
+    string s12(common12.begin(), common12.end());
+    return common_chars(s12, s3);
+}
+int priority(const set<char>& items) {
+    int result = 0;
+    for (char item : items) {
+        result += item >= 'a' ? item - 'a' + 1 : item - 'A' + 27;
+    }
+    return result;
+}
+
 int day03a(const vector<string>& lines) {
     int result = 0;
     for (const string& line : lines) {
         string compartment1 = line.substr(0, line.length() / 2);
         string compartment2 = line.substr(line.length() / 2);
-        set<char> compartment1items(compartment1.begin(), compartment1.end());
-        set<char> compartment2items(compartment2.begin(), compartment2.end());
-        set<char> common_items;
-        set_intersection(compartment1items.begin(), compartment1items.end(),
-                         compartment2items.begin(), compartment2items.end(),
-                         inserter(common_items, common_items.begin()));
-        for (char common_char : common_items) {
-            int priority = common_char >= 'a' ? common_char - 'a' + 1 : common_char - 'A' + 27;
-            result += priority;
-        }
+        result += priority(common_chars(compartment1, compartment2));
     }
     return result;
 }
 
 int day03b(const vector<string>& lines) {
     int result = 0;
-    set<char> common_items1, common_items2;
-    for (int i = 0; i < lines.size(); i++) {
-        if (i % 3 == 1) {
-            set<char> prev_items(lines[i-1].begin(), lines[i-1].end());
-            set<char> curr_items(lines[i].begin(), lines[i].end());
-            set_intersection(prev_items.begin(), prev_items.end(),
-                             curr_items.begin(), curr_items.end(),
-                             inserter(common_items1, common_items1.begin()));
-        }
-        if (i % 3 == 2) {
-            set<char> curr_items(lines[i].begin(), lines[i].end());
-            set_intersection(common_items1.begin(), common_items1.end(),
-                             curr_items.begin(), curr_items.end(),
-                             inserter(common_items2, common_items2.begin()));
-            for (char common_char : common_items2) {
-                int priority = common_char >= 'a' ? common_char - 'a' + 1 : common_char - 'A' + 27;
-                result += priority;
-            }
-            common_items1 = {};
-            common_items2 = {};
-        }
+    for (int i = 2; i < lines.size(); i += 3) {
+        result += priority(common_chars(lines[i-2], lines[i-1], lines[i]));
     }
     return result;
 }
