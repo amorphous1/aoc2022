66d78327b18b9f2ae7a2eea7664f5beeb39eacf8
amorphous1 <harrystew@gmail.com>
2022-12-06 19:36:37 +0000

    refactor day 6 to avoid duplication

--- day06.cpp
+++ day06.cpp
@@ -3,25 +3,23 @@
 
 using namespace std;
 
-int day06a(string input) {
-    for (int i = 3; i < input.length(); i++) {
-        set<char> last_four = { input[i], input[i-1], input[i-2], input[i-3] };
-        if (last_four.size() == 4) {
+int day06(const string& input, int num_distinct) {
+    for (int i = num_distinct - 1; i < input.length(); i++) {
+        set<char> last_chars = {};
+        for (int j = 0; j < num_distinct; j++) {
+            last_chars.insert(input[i-j]);
+        }
+        if (last_chars.size() == num_distinct) {
             return i + 1;
         }
     }
     return -1;
 }
 
-int day06b(string input) {
-    for (int i = 14 - 1; i < input.length(); i++) {
-        set<char> last_chars = {};
-        for (int j = 0; j < 14; j++) {
-            last_chars.insert(input[i-j]);
-        }
-        if (last_chars.size() == 14) {
-            return i + 1;
-        }
-    }
-    return -1;
+int day06a(const string& input) {
+    return day06(input, 4);
+}
+
+int day06b(const string& input) {
+    return day06(input, 14);
 }
