3d18889889c4e47d4ea9cf9f7df8a6ee98cda82d
amorphous1 <harrystew@gmail.com>
2022-12-09 15:57:37 +0000

    refactor to avoid duplication

--- day09.cpp
+++ day09.cpp
@@ -13,20 +13,11 @@ int sgn(int x) {
 struct Point {
     int x, y;
 
-    bool operator==(const Point& other) const {
-        return x == other.x && y == other.y;
-    }
     bool operator<(const Point& other) const {
         return x < other.x || (x == other.x && y < other.y);
     }
     Point operator+(const Point& other) const {
         return { other.x + x, other.y + y };
-    }
-    [[nodiscard]] Point add(const Point& other) const {
-        return { other.x + x, other.y + y };
-    }
-    [[nodiscard]] bool is_adjacent_to(const Point& other) const {
-        return abs(other.x - x) < 2 && abs(other.y - y) < 2;
     }
     [[nodiscard]] Point follow(const Point& other) const {
         if (abs(x - other.x) > 1 && abs(y - other.y) > 1) return { other.x + sgn(x - other.x), other.y + sgn(y - other.y) };
@@ -34,34 +25,12 @@ struct Point {
         if (abs(y - other.y) > 1) return { other.x, other.y + sgn(y - other.y) };
         return { x, y };
     }
-    [[nodiscard]] Point inverse() const {
-        return { -1 * x, -1 * y};
-    }
 };
 
-ulong day09a(const vector<string>& lines) {
-    const map<string, Point> directions = { {"R", {1,0}}, {"L", {-1,0}}, {"U", {0,1}}, {"D", {0,-1}} };
-    Point head = {0,0}, tail = {0,0};
-    set<Point> tail_positions = { tail };
-
-    for (const string& line : lines) {
-        vector<string> tokens = split(line, " ");
-        Point direction = directions.at(tokens[0]);
-        int steps = stoi(tokens[1]);
-        for (int step = 1; step <= steps; step++) {
-            head = head + direction;
-            if (!head.is_adjacent_to(tail)) {
-                tail = head + direction.inverse();
-                tail_positions.insert(tail);
-            }
-        }
-    }
-    return tail_positions.size();
-}
-
-ulong day09b(const vector<string>& lines) {
+ulong day09(const vector<string>& lines, int knots) {
     map<string, Point> directions = { {"R", {1,0}}, {"L", {-1,0}}, {"U", {0,1}}, {"D", {0,-1}} };
-    Point rope[] = { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0} };
+    vector<Point> rope;
+    for (int knot = 0; knot < knots; knot++) rope.push_back({0,0});
     set<Point> tail_positions = { {0,0} };
 
     for (const string& line : lines) {
@@ -70,11 +39,18 @@ ulong day09b(const vector<string>& lines) {
         int steps = stoi(tokens[1]);
         for (int step = 0; step < steps; step++) {
             rope[0] = rope[0] + direction;
-            for (int knot = 1; knot < 10; knot++) {
+            for (int knot = 1; knot < knots; knot++) {
                 rope[knot] = rope[knot].follow(rope[knot-1]);
             }
-            tail_positions.insert(rope[9]);
+            tail_positions.insert(rope[knots-1]);
         }
     }
     return tail_positions.size();
+}
+
+ulong day09a(const vector<string>& lines) {
+    return day09(lines, 2);
+}
+ulong day09b(const vector<string>& lines) {
+    return day09(lines, 10);
 }
