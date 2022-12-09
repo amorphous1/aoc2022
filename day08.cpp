72804f83bc7960d12a3f998dca4fddb0be97ad09
amorphous1 <harrystew@gmail.com>
2022-12-09 13:53:21 +0000

    minor rename and reformatting

--- day08.cpp
+++ day08.cpp
@@ -3,42 +3,38 @@
 
 using namespace std;
 
-bool all_smaller(int x, int y, int dx, int dy, const vector<string>& lines) {
-    int tree_height = lines.at(y)[x] - '0';
+bool all_smaller(int x, int y, int dx, int dy, const vector<string>& trees) {
+    int tree_height = trees.at(y)[x] - '0';
     int nx = x + dx, ny = y + dy;
-    while (ny >=0 && ny < lines.size() && nx >=0 && nx < lines.at(ny).size()) {
-        int other_height = lines.at(ny)[nx] - '0';
+    while (ny >=0 && ny < trees.size() && nx >= 0 && nx < trees.at(ny).size()) {
+        int other_height = trees.at(ny)[nx] - '0';
         if (other_height >= tree_height) return false;
         nx = nx + dx, ny = ny + dy;
     }
     return true;
 }
 
-bool is_visible(int x, int y, const vector<string>& lines) {
-    return all_smaller(x, y, -1, 0, lines) ||
-           all_smaller(x, y, 1, 0, lines) ||
-           all_smaller(x, y, 0, -1, lines) ||
-           all_smaller(x, y, 0, 1, lines);
+bool is_visible(int x, int y, const vector<string>& trees) {
+    return all_smaller(x, y, -1, 0, trees) || all_smaller(x, y, 1, 0, trees) ||
+           all_smaller(x, y, 0, -1, trees) || all_smaller(x, y, 0, 1, trees);
 }
 
-int scenic_score(int x, int y, int dx, int dy, const vector<string>& lines) {
-    int tree_height = lines.at(y)[x] - '0';
+int scenic_score(int x, int y, int dx, int dy, const vector<string>& trees) {
+    int tree_height = trees.at(y)[x] - '0';
     int nx = x + dx, ny = y + dy;
     int result = 0;
-    while (ny >=0 && ny < lines.size() && nx >=0 && nx < lines.at(ny).size()) {
+    while (ny >=0 && ny < trees.size() && nx >= 0 && nx < trees.at(ny).size()) {
         result++;
-        int other_height = lines.at(ny)[nx] - '0';
+        int other_height = trees.at(ny)[nx] - '0';
         if (other_height >= tree_height) return result;
         nx = nx + dx, ny = ny + dy;
     }
     return result;
 }
 
-int scenic_score(int x, int y, const vector<string>& lines) {
-    return scenic_score(x, y, -1, 0, lines) *
-           scenic_score(x, y, 1, 0, lines) *
-           scenic_score(x, y, 0, -1, lines) *
-           scenic_score(x, y, 0, 1, lines);
+int scenic_score(int x, int y, const vector<string>& trees) {
+    return scenic_score(x, y, -1, 0, trees) * scenic_score(x, y, 1, 0, trees) *
+           scenic_score(x, y, 0, -1, trees) * scenic_score(x, y, 0, 1, trees);
 }
 
 int day08a(const vector<string>& lines) {
