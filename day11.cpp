0bc4254d164316b7ab02a061cf662b31b5b66ac7
amorphous1 <harrystew@gmail.com>
2022-12-11 12:17:29 +0000

    ulong is long enough...

--- day11.cpp
+++ day11.cpp
@@ -7,22 +7,22 @@
 using namespace std;
 
 struct Monkey {
-    deque<unsigned long long> items_to_inspect;
+    deque<ulong> items_to_inspect;
     string worry_updater;
     int worry_test, worry_test_true_target, worry_test_false_target;
-    unsigned long long inspected_items = 0;
+    ulong inspected_items = 0;
 
-    unsigned long long inspect_next() {
-        unsigned long long old_worry = items_to_inspect.front();
+    ulong inspect_next_item() {
+        ulong old_worry = items_to_inspect.front();
         items_to_inspect.pop_front();
         inspected_items++;
-        auto operands = find_numbers<vector<unsigned long long>>(worry_updater);
-        unsigned long long operand = (operands.size() == 1) ? operands[0] : old_worry;
+        auto operands = find_numbers<vector<ulong>>(worry_updater);
+        ulong operand = (operands.size() == 1) ? operands[0] : old_worry;
         return worry_updater[0] == '*' ? old_worry * operand : old_worry + operand;
     }
 
     static Monkey parse(const vector<string>& monkey_spec, int start) {
-        auto items = find_numbers<deque<unsigned long long>>(monkey_spec[start + 1]);
+        auto items = find_numbers<deque<ulong>>(monkey_spec[start + 1]);
         string worry_updater = split(monkey_spec[start+2], " new = old ")[1];
         return { items, worry_updater, find_number(monkey_spec[start+3]),
                  find_number(monkey_spec[start+4]), find_number(monkey_spec[start+5]) };
@@ -33,9 +33,9 @@ struct Monkey {
     }
 };
 
-unsigned long long day11(const vector<string>& monkey_specs, int rounds, bool divide_worry) {
+ulong day11(const vector<string>& monkey_specs, int rounds, bool divide_worry) {
     vector<Monkey> monkeys;
-    unsigned long long multiplied_worry_tests = 1;
+    ulong multiplied_worry_tests = 1;
     for (int row = 0; row <= monkey_specs.size() - 6; row += 7) {
         const Monkey& monkey = Monkey::parse(monkey_specs, row);
         monkeys.push_back(monkey);
@@ -44,8 +44,8 @@ unsigned long long day11(const vector<string>& monkey_specs, int rounds, bool di
     for (int round = 1; round <= rounds; round++) {
         for (Monkey& monkey : monkeys) {
             while (!monkey.items_to_inspect.empty()) {
-                unsigned long long updated_worry = monkey.inspect_next();
-                unsigned long long new_worry = divide_worry ? updated_worry / 3 : updated_worry % multiplied_worry_tests;
+                ulong updated_worry = monkey.inspect_next_item();
+                ulong new_worry = divide_worry ? updated_worry / 3 : updated_worry % multiplied_worry_tests;
                 int target = new_worry % monkey.worry_test == 0 ? monkey.worry_test_true_target : monkey.worry_test_false_target;
                 monkeys[target].items_to_inspect.push_back(new_worry);
             }
@@ -55,10 +55,10 @@ unsigned long long day11(const vector<string>& monkey_specs, int rounds, bool di
     return monkeys[0].inspected_items * monkeys[1].inspected_items;
 }
 
-unsigned long long day11a(const vector<string>& monkey_specs) {
+ulong day11a(const vector<string>& monkey_specs) {
     return day11(monkey_specs, 20, true);
 }
 
-unsigned long long day11b(const vector<string>& monkey_specs) {
+ulong day11b(const vector<string>& monkey_specs) {
     return day11(monkey_specs, 10000, false);
 }
