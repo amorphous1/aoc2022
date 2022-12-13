4ead6508b48b402bad67019cf1b0c5b06eabde7e
amorphous1 <harrystew@gmail.com>
2022-12-13 22:36:58 +0000

    add day 13 part 2 solution

--- day13.cpp
+++ day13.cpp
@@ -1,8 +1,7 @@
+#include <algorithm>
 #include <deque>
 #include <string>
 #include <vector>
-#include <iostream>
-#include "common.h"
 
 using namespace std;
 
@@ -25,39 +24,28 @@ struct Packet {
         }
         return { tokens };
     }
-
-    friend ostream& operator<<(ostream& o, const Packet& p)
-    {
-        string text;
-        for (const int& token : p.tokens) {
-                text += to_string(token) + " ";
-        }
-        return o << text;
-    }
 };
 
-int compare(const Packet& packet1, const Packet& packet2) { // return -1/0/1 if packet1 is smaller/equal/bigger than packet2
-    deque<int> p1(packet1.tokens), p2(packet2.tokens);
-    while (!p1.empty() && !p2.empty()) {
-        int t1 = p1.front();
-        p1.pop_front();
-        int t2 = p2.front();
-        p2.pop_front();
+bool is_smaller(const Packet& packet1, const Packet& packet2) {  // return true if packet1 is smaller than packet2
+    deque<int> tokens1(packet1.tokens), tokens2(packet2.tokens);
+    while (!tokens1.empty() && !tokens2.empty()) {
+        int t1 = tokens1.front();
+        tokens1.pop_front();
+        int t2 = tokens2.front();
+        tokens2.pop_front();
         if (t1 == t2) continue;
-        if (t1 == Packet::LIST_END) return -1;  // packet1 smaller since a list in packet1 is shorter
-        if (t2 == Packet::LIST_END) return 1;   // packet2 bigger since a list in packet2 is shorter
-        if (t1 == Packet::LIST_START) {         // "package up" packet2's integer in a list
-            p2.push_front(Packet::LIST_END);
-            p2.push_front(t2);
-        } else if (t2 == Packet::LIST_START) {  // "package up" packet1's integer in a list
-            p1.push_front(Packet::LIST_END);
-            p1.push_front(t1);
+        if (t1 == Packet::LIST_END) return true;   // packet1 is smaller since a list in packet1 ends sooner
+        if (t2 == Packet::LIST_END) return false;  // packet1 is bigger since a list in packet2 ends sooner
+        if (t1 == Packet::LIST_START) {            // "package up" packet2's integer in a list
+            tokens2.push_front(Packet::LIST_END);
+            tokens2.push_front(t2);
+        } else if (t2 == Packet::LIST_START) {     // "package up" packet1's integer in a list
+            tokens1.push_front(Packet::LIST_END);
+            tokens1.push_front(t1);
         } else
-            return sgn(t1 - t2);                // at this point, both tokens must be different integers
+            return t1 < t2;                        // at this point, both tokens must be different integers
     }
-    if (!p1.empty()) return 1;                  // packet1 is bigger, because it contains more tokens
-    if (!p2.empty()) return -1;                 // packet1 is smaller, because it contains fewer tokens
-    return 0;
+    return tokens1.empty();
 }
 
 int day13a(const vector<string>& lines) {
@@ -67,9 +55,22 @@ int day13a(const vector<string>& lines) {
     }
     int result = 0;
     for (int i = 0; i < packet_pairs.size(); i++) {
-        cout << packet_pairs[i].first << endl << packet_pairs[i].second << endl;
-        cout << compare(packet_pairs[i].first, packet_pairs[i].second) << endl << endl;
-        result += compare(packet_pairs[i].first, packet_pairs[i].second) < 0 ? i + 1 : 0;
+        result += is_smaller(packet_pairs[i].first, packet_pairs[i].second) ? i + 1 : 0;
+    }
+    return result;
+}
+
+int day13b(const vector<string>& lines) {
+    Packet divider1 = Packet::parse("[[2]]"), divider2 = Packet::parse("[[6]]");
+    vector<Packet> packets = { divider1, divider2 };
+    for (int row = 0; row < lines.size(); row += 3) {
+        packets.push_back(Packet::parse(lines[row]));
+        packets.push_back(Packet::parse(lines[row+1]));
+    }
+    sort(packets.begin(), packets.end(), is_smaller);
+    int result = 1;
+    for (int i = 0; i < packets.size(); i++) {
+        result *= (packets[i].tokens == divider1.tokens || packets[i].tokens == divider2.tokens) ? i + 1 : 1;
     }
     return result;
 }
--- main.cpp
+++ main.cpp
@@ -69,6 +69,7 @@ int main() {
 //    cout << day11b(read_lines("inputs/day11.input")) << endl;  // 35270398814
 //    cout << day12a(read_lines("inputs/day12.input")) << endl;  // 383
 //    cout << day12b(read_lines("inputs/day12.input")) << endl;  // 377
-    cout << day13a(read_lines("inputs/day13.input")) << endl;  //
+    cout << day13a(read_lines("inputs/day13.input")) << endl;  // 6272
+    cout << day13b(read_lines("inputs/day13.input")) << endl;  //
     return 0;
 }
